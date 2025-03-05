59061d173a153c2ef47bbdf8d1ee9a469f98f64c
Stas <amosov-stas@mail.ru>
2025-03-05 15:25:47 +0300

    validate arguments length + test + githooks

--- internal/database/compute/errors.go
+++ internal/database/compute/errors.go
@@ -5,6 +5,5 @@ import "errors"
 var (
 	errInvalidLogger      = errors.New("invalid logger")
 	errNotEnoughArguments = errors.New("not enought arguments")
-	errInvalidArgument    = errors.New("invalid argument")
 	errUnknownCommandType = errors.New("unknown command type")
 )
--- readme.md
+++ readme.md
@@ -6,4 +6,6 @@ https://vektra.github.io/mockery/latest/
 
 ## Git hooks
 
+Install golangcli-lint: `go install golang.org/x/lint/golint@latest`
+
 Run command: `git config core.hooksPath .githooks`
